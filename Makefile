5104657ed75e866364a02edbf56d9fd3166aedd5
dora-explora <sarrafdara@gmail.com>
2026-07-30 14:21:09 -0700

    added miniaudio and audio file playback

--- Makefile
+++ Makefile
@@ -1,11 +1,10 @@
 all: src/main.c
-	gcc -o build/trackoverflow src/main.c
+	mkdir -p build
+	gcc \
+	    -Iminiaudio \
+		src/main.c miniaudio/miniaudio.c \
+		-o build/trackoverflow \
+		-pthread -lm
 
-r: all
+run: all
 	build/trackoverflow
-
-w: src/main.c
-	x86_64-w64-mingw32-gcc -o build/trackoverflow.exe src/main.c
-
-rw: w
-	wine build/trackoverflow.exe
--- src/main.c
+++ src/main.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
 #include <stdint.h>
+#include "../miniaudio/miniaudio.h"
 
 int main(int argc, char** argv) {
+    ma_result result;
+    ma_engine engine;
+
+    result = ma_engine_init(NULL, &engine);
+    if (result != MA_SUCCESS) {
+        puts("failed to initialize audio engine");
+        return -1;
+    }
+
+    ma_engine_play_sound(&engine, "assets/bubble.mp3", NULL);
+    puts("press anything to leave");
+    getchar();
+
+    ma_engine_uninit(&engine);
+    return 0;
+
     uint64_t data; // variable to store each word we read but also to find the stack
     uint64_t* start = &data; // pointer to the data variable, which is near the start of the stack
-    for (uint64_t i = 0; 1; data = *(start + i++)) { // i = -80 because data is interesting for ~80 bytes before &data
+    for (uint64_t i = 0; 1; data = *(start + i++)) {
         // data is accessed in 64 bit chunks, taking from i words after the start
         if (data == 0) { continue; } // if the word is all zeroes we dont care so skip it
         printf("0x%04lX 0x%016lX\n", i, data); // print index and data, 2 and 8 bytes of hex respectively
@@ -18,5 +35,6 @@ int main(int argc, char** argv) {
             #endif
         #endif
     }
+
     return 0;
 }
